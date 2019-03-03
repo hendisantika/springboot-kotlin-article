93300764c044545e2592d88097950b04c803cab1
hendisantika <hendisantika@yahoo.co.id>
2019-03-04 06:16:39 +0700

    #3 - Add Article entity

---
+++ src/main/kotlin/com/hendisantika/springbootkotlinarticle/entity/Article.kt
@@ -0,0 +1,27 @@
+package com.hendisantika.springbootkotlinarticle.entity
+
+import javax.persistence.*
+import javax.validation.constraints.NotBlank
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-kotlin-article
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-04
+ * Time: 06:16
+ */
+@Entity
+@Table(name = "articles")
+data class Article(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "article_id")
+        val id: Int = 0,
+
+        @get: NotBlank
+        val title: String = "",
+
+        @get: NotBlank
+        val contents: String = ""
+)
