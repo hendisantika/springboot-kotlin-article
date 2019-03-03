1bc55efb4cf067f2b7f32fc9458611c59fffc778
hendisantika <hendisantika@yahoo.co.id>
2019-03-04 06:54:28 +0700

    #22 - Add Unit Test

--- src/test/kotlin/com/hendisantika/springbootkotlinarticle/SpringbootKotlinArticleApplicationTests.kt
+++ src/test/kotlin/com/hendisantika/springbootkotlinarticle/SpringbootKotlinArticleApplicationTests.kt
@@ -1,16 +1,29 @@
 package com.hendisantika.springbootkotlinarticle
 
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringbootKotlinArticleApplicationTests {
 
+    @Autowired
+    lateinit var testRestTemplate: TestRestTemplate
+
     @Test
-    fun contextLoads() {
+    fun getAllArticles() {
+        //	val content = """[{"article_id":1,"title":"Learning Spring Boot","contents":"Some resources to learn Spring Boot"}]"""
+        val result = testRestTemplate.getForEntity("/api/articles", String::class.java)
+        assertNotNull(result)
+        assertEquals(result?.statusCode, HttpStatus.OK)
+        //	assertEquals(result?.body, content)
     }
 
 }
