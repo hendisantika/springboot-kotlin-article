b1a82d005c16bed38348134c9dec0a7e6b0d2eb9
hendisantika <hendisantika@yahoo.co.id>
2019-03-04 06:12:55 +0700

    #1 - Initial commit

---
+++ src/main/kotlin/com/hendisantika/springbootkotlinarticle/SpringbootKotlinArticleApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.springbootkotlinarticle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinArticleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootKotlinArticleApplication>(*args)
+}
